a578015e9cbb38408235a341e8facccb2cdf8500
Zhong Zhenyu <nczzy1997@gmail.com>
2018-10-21 20:59:50 +0800

    尝试用 Qt 5.11.2 编译游戏

--- controller/controller_version.cpp
+++ controller/controller_version.cpp
@@ -3,7 +3,7 @@
 /**
  * \brief 版本号
  */
-const QString VersionController::VERSION_NUMBER = QString("2.8.0.3");
+const QString VersionController::VERSION_NUMBER = QString("2.8.0.4");
 
 /**
  * \brief 编译日期
